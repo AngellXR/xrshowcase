168d0427a741d8fcf90348bde19d20e3acd292fb
Jesse <63426722+jsealt@users.noreply.github.com>
2020-12-04 22:43:01 -0500

    connected figma
    not sure how, but its connected. Now I have to figure out why its called "my story"

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,5 +1,19 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Card from "./Card.jsx";
+import { withDesign } from 'storybook-addon-designs'
 
-storiesOf("Card", module).add("default", () => <Card />);
+export default {
+  title: 'Card',
+  decorators: [withDesign],
+}
+
+export const myStory = () => <Card />
+
+myStory.parameters = {
+  design: {
+    type: 'figma',
+    url:
+      'https://www.figma.com/file/7inAfhaj1vHLtamgaiXSkU/AngellXR-sandbox?node-id=112%3A915',
+  },
+}
