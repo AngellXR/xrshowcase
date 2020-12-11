de5925b766c56b7a67159b16f89b15741e94b04c
jsealt <jsealt@users.noreply.github.com>
2020-12-11 18:41:30 -0500

    changed default sb background
    used brand gray 333333

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  backgrounds: {
+    default: 'gray',
+    values: [
+      {
+        name: 'gray',
+        value: '#333333',
+      },
+      // {
+      //   name: 'facebook',
+      //   value: '#3b5998',
+      // },
+    ],
+  },
+};
