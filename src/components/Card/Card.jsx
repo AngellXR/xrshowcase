69717b4aeb6155a72b45d2e102e463b6bae3dfe6
Jesse <63426722+jsealt@users.noreply.github.com>
2020-12-16 15:26:54 -0500

    commit to share

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,29 +5,36 @@ import CardGif from '../CardGif/CardGif.jsx';
 
 
 const StyledCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 0px;
 
-    position: relative;
-    width: 18.75em;
-    height: 25.8125em;
-    left: calc(50% - 18.75em/2);
-    top: calc(50% - 25.8125em/2 - 0.5px);
+width: 32%;
+padding-bottom: 3%; /* Same as width, sets height */
+padding-top: 3%; /* Same as width, sets height */
+margin-bottom: 2%; /* (100-32*3)/2 */
 
-    background: #333333;
-    border: 10px solid #000000;
-    box-shadow: 1.25em 1.25em .25em #623CEA;
-    border-radius: 1.25em;
-    
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 5px;
+
+position: relative;
+width: 18.75em;
+height: 25.8125em;
+left: calc(50% - 18.75em / 2);
+top: calc(50% - 25.8125em / 2 - 0.5px);
+
+background: #333333;
+border: 10px solid #000000;
+box-shadow: 1.25em 1.25em 0.25em #623cea;
+border-radius: 1.25em;
 `
 
 export default function Card({ cardInfo }) {
     return (
         <StyledCard>
-                <CardGif imgURL={cardInfo.imgURL}/>
-            <CardBody title={cardInfo.title} description={cardInfo.description}/>
+            <div>
+                    <CardGif imgURL={cardInfo.imgURL}/>
+                <CardBody title={cardInfo.title} description={cardInfo.description}/>
+            </div>
         </StyledCard>
     );
 }
